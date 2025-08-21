3c398f6dc71be74b4ff0911e917b890d38f979d5
İsmail ERSÖZ <94225961+ersoz12@users.noreply.github.com>
2025-08-21 05:02:26 +0300

    Kod1.java

--- src/Kod1.java
+++ src/Kod1.java
@@ -1,34 +1,29 @@
-/**
- * Kod1.java - OOP Başlangıç Seviyesi Örneği
- * Öğrenci sınıfı ile temel OOP kavramları
- */
+
 public class Kod1 {
     public static void main(String[] args) {
-        // Öğrenci nesneleri oluşturma
+     
         Ogrenci ogrenci1 = new Ogrenci("Ahmet", 20, "Bilgisayar Mühendisliği");
         Ogrenci ogrenci2 = new Ogrenci("Ayşe", 19, "Matematik");
         
-        // Öğrenci bilgilerini yazdırma
+        
         ogrenci1.bilgileriGoster();
         ogrenci2.bilgileriGoster();
         
-        // Öğrenci yaşını güncelleme
+        
         ogrenci1.yasGuncelle(21);
         System.out.println("\nGüncellenmiş bilgiler:");
         ogrenci1.bilgileriGoster();
     }
 }
 
-/**
- * Öğrenci sınıfı - OOP temel kavramları
- */
+
 class Ogrenci {
-    // Sınıf değişkenleri (fields)
+   
     private String ad;
     private int yas;
     private String bolum;
     
-    // Constructor (Yapıcı metod)
+   
     public Ogrenci(String ad, int yas, String bolum) {
         this.ad = ad;
         this.yas = yas;
@@ -36,7 +31,7 @@ class Ogrenci {
         System.out.println("Yeni öğrenci oluşturuldu: " + ad);
     }
     
-    // Getter metodları
+   
     public String getAd() {
         return ad;
     }
@@ -49,13 +44,13 @@ class Ogrenci {
         return bolum;
     }
     
-    // Setter metodları
+   
     public void yasGuncelle(int yeniYas) {
         this.yas = yeniYas;
         System.out.println(ad + " adlı öğrencinin yaşı " + yeniYas + " olarak güncellendi.");
     }
     
-    // Bilgileri gösterme metodu
+ 
     public void bilgileriGoster() {
         System.out.println("Öğrenci Bilgileri:");
         System.out.println("Ad: " + ad);
